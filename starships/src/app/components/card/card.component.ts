27048525b1eea6d024e4d261b471f32cc93db3d9
Maks <makzylinski@gmail.com>
2024-06-05 18:53:02 +0200

    Displayed data in the template, added utils method

--- starships/src/app/components/card/card.component.ts
+++ starships/src/app/components/card/card.component.ts
@@ -1,7 +1,8 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
-import { Observable } from 'rxjs';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { Observable, take, tap } from 'rxjs';
 import { Person } from 'src/app/models/person';
 import { Starship } from 'src/app/models/starship';
+import { transformAttributesToListItems } from 'src/app/utils/attributes.utils';
 
 @Component({
   selector: 'app-card',
@@ -9,7 +10,18 @@ import { Starship } from 'src/app/models/starship';
   styleUrl: './card.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
   @Input() entity$: Observable<any>;
+  attributesListItems: any;
+
+
+  ngOnInit(): void {
+    this.entity$.pipe(
+      take(1),
+      tap((entityInfo) => 
+        this.attributesListItems = transformAttributesToListItems(entityInfo)
+      )
+    ).subscribe();
+  }
 
 }
