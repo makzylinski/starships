f50ea0697a89e03d3015defc16cefafd54e5d9df
Maks <makzylinski@gmail.com>
2024-06-05 19:20:36 +0200

    Added entity switcher template and styles

--- starships/src/app/app.module.ts
+++ starships/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CardComponent } from './components/card/card.component';
 import { EntitySwitcherComponent } from './components/entity-switcher/entity-switcher.component';
 
 const COMPONENTS = [AppComponent, CardComponent, EntitySwitcherComponent];
-const IMPORTS = [BrowserModule, HttpClientModule, MatCardModule, MatButtonModule];
+const IMPORTS = [BrowserModule, HttpClientModule, MatCardModule, MatButtonModule, MatIconModule];
 
 @NgModule({
   declarations: COMPONENTS,
