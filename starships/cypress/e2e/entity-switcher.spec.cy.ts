38a1461c9dfe874295616772288980bf3e868ee2
Maks <makzylinski@gmail.com>
2024-06-09 21:08:36 +0200

    entity switcher tests

--- starships/cypress/e2e/entity-switcher.spec.cy.ts
+++ starships/cypress/e2e/entity-switcher.spec.cy.ts
@@ -5,9 +5,25 @@ before(() => {
 })
 
 describe('Entity Switcher Tests', () => {
-    it('check elements visibilty', () => {
+    it('features', () => {
+        cy.log('check visibility');
         entitySwitcherSelectors.entitySwitcherContainer().should('be.visible');
         entitySwitcherSelectors.entitySwitcherPeople().should('be.visible');
         entitySwitcherSelectors.entitySwitcherStarships().should('be.visible');
+
+        cy.log('check type switch');
+        cy.intercept('GET', 'https://www.swapi.tech/api/people/**').as('peopleRequest');
+        entitySwitcherSelectors.entitySwitcherPeople().click()
+        
+        cy.wait('@peopleRequest').should(({ request }) => {
+        expect(request.url).to.include('https://www.swapi.tech/api/people/');
+        });
+
+        cy.intercept('GET', 'https://www.swapi.tech/api/starships/**').as('starshipsRequest');
+        entitySwitcherSelectors.entitySwitcherStarships().click()
+        
+        cy.wait('@starshipsRequest').should(({ request }) => {
+        expect(request.url).to.include('https://www.swapi.tech/api/starships/');
+        });
     })
 })
