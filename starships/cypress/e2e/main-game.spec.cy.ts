6be80a81c9110e8bfc1308a2ec57f2b4407cc9c9
Maks <makzylinski@gmail.com>
2024-06-09 18:50:24 +0200

    main game tests

--- starships/cypress/e2e/main-game.spec.cy.ts
+++ starships/cypress/e2e/main-game.spec.cy.ts
@@ -1,11 +1,14 @@
 import { mainGameSelectors } from "cypress/selectors/main-game.selectors";
 
 before(() => {
-    cy.visit('http://localhost:4200').wait(3000);
+    cy.visit('http://localhost:4200');
 })
 
 describe('Main game window test', () => {
     it('Should test elements visibility', () => {
         mainGameSelectors.mainGamePlayAgainButton().should('be.visible');
+        mainGameSelectors.mainGameWinner().should('be.visible');
+        mainGameSelectors.mainGamePlayerOne().should('be.visible');
+        mainGameSelectors.mainGamePlayerTwo().should('be.visible');
     })
 })
