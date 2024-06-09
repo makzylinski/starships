e8e61c19adf622665d3e4e469555bd2f9dc6fdc7
Maks <makzylinski@gmail.com>
2024-06-09 20:45:27 +0200

    added additional tests to main game

--- starships/cypress/e2e/main-game.spec.cy.ts
+++ starships/cypress/e2e/main-game.spec.cy.ts
@@ -1,14 +1,45 @@
 import { mainGameSelectors } from "cypress/selectors/main-game.selectors";
 
-before(() => {
+beforeEach(() => {
     cy.visit('http://localhost:4200');
+});
+
+after(() => {
+    cy.clearLocalStorage();
 })
 
 describe('Main game window test', () => {
-    it('Should test elements visibility', () => {
+    it('Features', () => {
         mainGameSelectors.mainGamePlayAgainButton().should('be.visible');
         mainGameSelectors.mainGameWinner().should('be.visible');
         mainGameSelectors.mainGamePlayerOne().should('be.visible');
         mainGameSelectors.mainGamePlayerTwo().should('be.visible');
-    })
-})
+   
+        cy.window().its('localStorage').then(localStorage => {
+            const player1 = Number(localStorage.getItem('Player 1')) ?? 0;
+            const player2 = Number(localStorage.getItem('Player 2')) ?? 0;
+
+            console.log(player1, player2)
+
+            if (player1 > player2) {
+                mainGameSelectors.mainGameWinner().invoke('text').then((text) => {
+                    const cleanText = text.trim();
+                    expect(cleanText).to.equal('The winner is... Player 1');
+                    mainGameSelectors.mainGamePlayerOne().should('contain.text', 'Player One: 1');
+                });
+            } else if (player2 > player1) {
+                mainGameSelectors.mainGameWinner().invoke('text').then((text) => {
+                    const cleanText = text.trim();
+                    expect(cleanText).to.equal('The winner is... Player 2');
+                    mainGameSelectors.mainGamePlayerOne().should('contain.text', 'Player Two: 1');
+                });
+            } else {
+                
+                mainGameSelectors.mainGameWinner().invoke('text').then((text) => {
+                    const cleanText = text.trim();
+                    expect(cleanText).to.equal('The winner is... Draw or no data');
+                });
+            }
+        });
+    });
+});
