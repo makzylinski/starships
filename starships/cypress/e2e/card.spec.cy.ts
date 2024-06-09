997c238157c83543b5e76d1c0f2e9f500210f119
Maks <makzylinski@gmail.com>
2024-06-09 21:05:40 +0200

    card tests

--- starships/cypress/e2e/card.spec.cy.ts
+++ starships/cypress/e2e/card.spec.cy.ts
@@ -5,9 +5,14 @@ before(() => {
 })
 
 describe('Card Tests', () => {
-    it('should test elements visibility', () => {
+    it('features', () => {
+        cy.log('check visibility');
         cardSelectors.cardContainer().should('be.visible');
         cardSelectors.cardName().should('be.visible');
         cardSelectors.cardList().should('be.visible');
+    
+        cy.log('check the name');
+        cardSelectors.cardName().eq(0).should('contain.text', 'Player 1');
+        cardSelectors.cardName().eq(1).should('contain.text', 'Player 2');
     })
 })
